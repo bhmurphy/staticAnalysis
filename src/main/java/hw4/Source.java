c7e31aef4155e232b7b0a49b08ab53a55ff18a2f
bhmurphy <bhm5zr@virginia.edu>
2019-10-23 18:27:08 -0400

    first commit

---
+++ src/main/java/hw4/Source.java
@@ -0,0 +1,11 @@
+package hw4;
+
+public class Source {
+    public static void main(String[] args) {
+        String sample = "foo bar";
+        if (sample == sample)
+            System.out.println(sample.length());
+        if (sample == "Wow")
+            System.out.println("Yep");
+    }
+}
